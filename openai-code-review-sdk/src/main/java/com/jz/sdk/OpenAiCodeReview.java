6fbe9665986364707bb2f499b581f18565e16891
meizhihao <meizhihao@millionlab.cn>
2025-03-13 18:24:47 +0800

    feat-#m-maven: 测试代码

--- openai-code-review-sdk/src/main/java/com/jz/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/com/jz/sdk/OpenAiCodeReview.java
@@ -1,5 +1,10 @@
 package com.jz.sdk;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * @author meizhihao@millionlab.cn
  * @date 2025-03-12 18:09
@@ -7,10 +12,29 @@ package com.jz.sdk;
 public class OpenAiCodeReview {
 
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException, InterruptedException {
 
 
     System.out.println("测试执行");
+
+    // 1. 代码检出
+    ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "HEAD~1", "HEAD");
+    processBuilder.directory(new File("."));
+
+    Process process = processBuilder.start();
+
+    BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+    String line;
+
+    StringBuilder diffCode = new StringBuilder();
+    while ((line = reader.readLine()) != null) {
+      diffCode.append(line);
+    }
+
+    int exitCode = process.waitFor();
+    System.out.println("Exited with code:" + exitCode);
+
+    System.out.println("评审代码：" + diffCode.toString());
   }
 
 
